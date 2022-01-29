e287996749690ade1ccbe86e862fd77d780c2304
Martin W <proggarmartin@gmail.com>
2022-01-29 08:24:29 +0100

    Add Book-entity to core project

---
+++ Boox/Boox.Core/Entities/Book.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core.Entities
+{
+    public class Book
+    {
+        [Key]
+        public string Id { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+        public string Genre { get; set; }
+        public double Price { get; set; }
+        public DateTime Published { get; set; }
+        public string Title { get; set; }
+    }
+}
