64bd7431ab9155aefa82ee3b0a936ae4429f0949
Martin W <proggarmartin@gmail.com>
2022-01-29 08:25:22 +0100

    Add BooxContext to infrastructure/data

---
+++ Boox/Boox.Infrastructure/Data/BooxContext.cs
@@ -0,0 +1,23 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boox.Infrastructure.Data
+{
+    public class BooxContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            var dbPath = Path.Join(path, "BooxDb");
+            options.UseSqlite($"Data Source={dbPath}");
+        }
+    }
+}
